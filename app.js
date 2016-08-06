278aab06041e12a9fcfd92f982dcb613b79da021
Deokju Hong <Deokju@Deokjuui-MacBook-Air.local>
2016-08-06 16:19:34 -0700

    app.js configuration

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use('/', routes);
 
 app.set('port', process.env.PORT || 8000);
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views'.createEngine()));
+
 var server = app.listen(app.get('port'), () => {
     console.log('app started');
 });
