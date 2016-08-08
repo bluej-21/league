67b90e87af8ed21738cd2ca1fb70f9e54cbcbda1
isthisnagee <nagee.elghassein@mail.utoronto.ca>
2016-08-07 23:09:02 -0700

    connection to db succesful!

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017');
+
 var routes = require('./src/routes/index');
+
+// database
+// ------
+var Game = require('./models/game');
+var League = require('./models/league');
+var Player = require('./models/player');
+var db  = mongoose.connection;
+
+// test database
+// ------
+db.on('error', console.error.bind(console, 'connection error'));
+db.on('open', () => {
+	console.log('connected!');
+});
 
 var app = express();
 
