9cfb539b2c1ffc1bda2ac942bb4fec28712302a6
isthisnagee <nagee.elghassein@mail.utoronto.ca>
2016-08-08 18:59:28 -0700

    added port variable

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ var express = require('express');
 var router = require('./src/routes/routes');
 
 var app = express();
+var PORT = process.env.PORT || 8000;
 
 app.use('/', router);
 
-app.set('port', process.env.PORT || 8000);
+app.set('port', PORT);
 
 app.set('views', __dirname + '/src/templates');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 var server = app.listen(app.get('port'), () => {
-    console.log('app started');
+    console.log('app started on http://localhost:' + PORT);
 });
 
