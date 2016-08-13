e7e8fa16423687ae53a3acb003a470d7d7bb0647
isthisnagee <nagee.elghassein@mail.utoronto.ca>
2016-08-13 16:34:21 -0700

    api basics

--- src/app.js
+++ src/app.js
@@ -1,29 +1,12 @@
 var express = require('express');
-var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/league');
 
 var routes = require('./routes/routes');
 var api = require('./routes/api');
 
-/*
- * database
- */
-var Game = require('./models/game');
-var League = require('./models/league');
-var Player = require('./models/player');
-var db  = mongoose.connection;
-
-/*
- * test database
- */
-// db.on('error', console.error.bind(console, 'connection error'));
-// db.on('open', () => {
-// 	console.log('connected!');
-// });
 
 var app = express();
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/api', api);
 
 app.set('port', process.env.PORT || 8000);
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,56 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost:27017/league');
+
+var Game = require('../models/game');
+var League = require('../models/league');
+var Player = require('../models/player');
+
+var db  = mongoose.connection;
+
+var config = require('../../config');
 var apiRouter = express.Router();
 
-apiRouter.get('/api', (req, res) => {
-  res.send('This is the league api');
+/*
+ * apiRouter settings 
+ */
+apiRouter.use( bodyParser.urlencoded({ extended: true }) );
+apiRouter.use( bodyParser.json() );
+/*
+ * test database
+ */
+
+db.on('error', console.error.bind(console, 'connection error'));
+db.on('open', () => {
+	console.log('connected!');
+});
+
+/*
+ * GETS
+ */
+
+// GET: /api/
+apiRouter.get('/', (req, res) => {
+  res.send({
+    message: 'This is the league api'
+  });
+});
+
+// GET: /api/games/
+apiRouter.get('/games/', (req, res) => {
+  res.send({
+    games: 'games'
+  });
+});
+
+// GET: /api/games/:game_id
+apiRouter.get('/games/:game_id', (req, res) => {
+  res.send({
+    id: req.params.game_id
+  });
 });
 
 module.exports = apiRouter;
